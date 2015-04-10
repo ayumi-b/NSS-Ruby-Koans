966c2fd2b45c88a710b23334741d7d22d574b715
Ayumi Fukuda Bennett <ayumi.fukuda@gmail.com>
2015-04-10 10:44:29 -0500

    triangleError rules, completed by need more learning

--- about_triangle_project_2.rb
+++ about_triangle_project_2.rb
@@ -6,6 +6,18 @@ require './triangle.rb'
 class AboutTriangleProject2 < Neo::Koan
   # The first assignment did not talk about how to handle errors.
   # Let's handle that part now.
+
+  def triangle(a, b, c)
+    raise TriangleError, "All Sides must be greater than zero" if (a == 0) | (b == 0) | (c == 0)
+    raise TriangleError, "Sides cannot be a negative number" if (a < 0) | (b < 0) | (c < 0)
+    raise TriangleError, "rule: Two sides can never be less than the sum of one side" if ((a + b) < c) | ((a + c) < b) | ((b + c) < a)
+    raise TriangleError, "rule: Two sides can never be equal one side" if ((a + b) ==  c) | ((a + c) ==  b) | ((b + c) ==  a)
+    #return :equilateral if (a == b) & (a == c) & (b == c)
+    #return :isosceles if (a == b) | (a == c) | (b == c)
+    #return :scalene
+  end
+
+
   def test_illegal_triangles_throw_exceptions
     assert_raise(TriangleError) do triangle(0, 0, 0) end
     assert_raise(TriangleError) do triangle(3, 4, -5) end
